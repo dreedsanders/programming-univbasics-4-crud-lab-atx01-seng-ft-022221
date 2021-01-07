3d4105aa70a76749db9f94e06d9345d0056bc486
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-07 19:33:48 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,66 @@
-def create_an_empty_array
+def create_an_empty_array 
+ []
+
+end
   
+
+def create_an_array  
+  ["apple", "tomato", "orange", "avocado"]
+
 end
 
-def create_an_array
+
+def add_element_to_end_of_array
+  ["Wow", "I", "am", "really", "learning"]
   
-end
+add_element_to_end_of_array<< "arrays!"
 
-def add_element_to_end_of_array(array, element)
+end
   
-end
 
-def add_element_to_start_of_array(array, element)
+def add_element_to_start_of_array =  ["I", "am", "really", "learning"]
+   
+add_element_to_start_of_array.unshift "wow"
+end
   
-end
 
-def remove_element_from_end_of_array(array)
+def remove_element_from_end_of_array 
+  ["I", "am", "really", "learning", "arrays!"]
   
-end
+  arrays = remove_element_from_end_of_array.pop 
+  end
 
-def remove_element_from_start_of_array(array)
+
+def remove_element_from_start_of_array
+  ["wow", "I", "am", "really", "learning", "arrays!" ]
+  wow=remove_element_from_start_of_array.shift
+  end
+
+
+def retrieve_element_from_index
+  ["wow", "I", "am", "really", "learning", "arrays!" ]
+
+retrieve_element_from_index[2]
+  end
+
+
+def retrieve_first_element_from_array
+  ["wow", "I", "am", "really", "learning", "arrays!" ]
   
-end
+  retrieve_first_element_from_array[0]
+  end
 
-def retrieve_element_from_index(array, index_number)
+
+def retrieve_last_element_from_array
+  ["wow", "I", "am", "really", "learning", "arrays!" ]
+
   
-end
+  retrieve_last_element_from_array[-1]
+  end
 
-def retrieve_first_element_from_array(array)
+
+def update_element_from_index
+  ["wow", "I", "am", "really", "learning", "arrays!" ]
   
-end
-
-def retrieve_last_element_from_array(array)
-  
-end
-
-def update_element_from_index(array, index_number, element)
-
+  update_element_from_index[4]="totally"
 end
